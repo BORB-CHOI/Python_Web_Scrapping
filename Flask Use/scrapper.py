acc54c7c199232b9daf712742c55c0a6fc9daad7
Your Name <you@example.com>
2020-05-17 02:11:11 +0900

    Scrapping site

--- Flask Use/scrapper.py
+++ Flask Use/scrapper.py
@@ -1,11 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://stackoverflow.com/jobs?q=python"
 
-
-def get_last_page():
-    result = requests.get(URL)
+def get_last_page(url):
+    result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", class_="s-pagination").find_all("a")
     last_page = pages[-2].get_text(strip=True)
@@ -22,11 +20,11 @@ def extract_job(job_html):
     return {'title': title, 'company': company, 'location': location, 'link': f"https://stackoverflow.com/jobs/{job_id}"}
 
 
-def extract_jobs(last_page):
+def extract_jobs(last_page, url):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
-        result = requests.get(f"{URL}&pg={page+1}")
+        result = requests.get(f"{url}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", class_="-job")
         for result in results:
@@ -35,7 +33,8 @@ def extract_jobs(last_page):
     return jobs
 
 
-def get_jobs():
-    last_page = get_last_page()
-    jobs = extract_jobs(last_page)
+def get_jobs(word):
+    url = f"https://stackoverflow.com/jobs?q={word}"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
     return jobs
